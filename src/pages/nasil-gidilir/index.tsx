ae184ad9f82b65de67da205a53d3630e06abbaee
Violetbee <caglarkarahuseyin28@gmail.com>
2023-05-31 18:28:25 +0300

    some texts was updated

--- src/pages/nasil-gidilir/index.tsx
+++ src/pages/nasil-gidilir/index.tsx
@@ -31,14 +31,19 @@ function index() {
             <div className="flex flex-col gap-5">
               <p className="text-lg font-medium">
                 Adanın merkezinde bulunan iskeledeki teknelerle ulaşım
-                sağlanmaktadır. İstanbul’dan Avşa Adası’na ulaşım için Yenikapı
-                – Avşa Adası seferlerini kullanabilirsiniz. İstanbul’dan Avşa
-                Adası’na ulaşım için Yenikapı – Avşa Adası seferlerini
-                kullanabilirsiniz.
+                sağlanmaktadır.
               </p>
-              <p>
+              <p className="text-lg font-medium">
+                İstanbul’dan Avşa Adası’na ulaşım için Yenikapı - Avşa, Bostancı
+                - Avşa ve Avcılar - Avşa İDO seferlerini kullanabilirsiniz.
+              </p>
+              <p className="text-lg font-medium">
                 Tekirdağ iskelesi üzerinden de her gün Avşa Adası’na ulaşım
                 sağlayabilirsiniz.
+              </p>
+              <p className="text-lg font-medium">
+                Balıkesir Erdek iskelesi üzerinden de her gün Avşa Adası’na
+                ulaşım sağlayabilirsiniz.
               </p>
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3033.351023618179!2d27.496068815398726!3d40.511731779354214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDDCsDMwJzQyLjIiTiAyN8KwMjknNTMuNyJF!5e0!3m2!1str!2str!4v1685181746791!5m2!1str!2str"
