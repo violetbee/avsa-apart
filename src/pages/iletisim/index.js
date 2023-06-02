9a645ff8d3cbd3764da1cff84146d4cc657edd18
Violetbee <caglarkarahuseyin28@gmail.com>
2023-06-03 00:04:01 +0300

    mail ok

--- src/pages/iletisim/index.js
+++ src/pages/iletisim/index.js
@@ -1,12 +1,16 @@
 import Layout from '@/components/Layout';
+import { useState } from 'react';
 
 function Index() {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [senderMail, setSenderMail] = useState('');
   return (
     <Layout>
       <div className='container mx-auto mt-3 max-w-[1300px] rounded-md bg-white p-1'>
         <div className='flex flex-col justify-between gap-10 rounded-md border-[2px] border-dashed border-black/10 px-10 py-6 md:flex-row'>
           {/* Contact Page, Including map and address */}
-          <div className='flex md:flex-row flex-col justify-center gap-10 text-zinc-900'>
+          <div className='flex md:flex-row flex-col justify-center w-full gap-10 text-zinc-900'>
             <div className='flex flex-col gap-5 mt-5'>
               <h1 className='text-4xl font-bold'>Adres</h1>
               <p className='text-lg font-medium'>
@@ -20,17 +24,56 @@ function Index() {
                 <a href='tel:+905323456789'>+90 (536) 698 34 53</a>
               </p>
             </div>
-            <div className='relative w-full'>
-              <iframe
-                src='https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3033.351023618179!2d27.496068815398726!3d40.511731779354214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDDCsDMwJzQyLjIiTiAyN8KwMjknNTMuNyJF!5e0!3m2!1str!2str!4v1685181746791!5m2!1str!2str'
-                className='h-[500px] w-full rounded-md'
-                style={{ border: 0 }}
-                allowFullScreen
-                loading='lazy'
-              ></iframe>
-            </div>
+            {/* Form area */}
+            <div className='flex flex-col max-w-md gap-5 w-full'>
+              <h1 className='text-4xl font-bold'>İletişim Formu</h1>
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
 
-            {/* Address */}
+                  window.open(
+                    `mailto:
+                    mahirapartavsa@gmail.com
+                      ?subject=${title}
+                      &body=${body}
+                      &sender=${senderMail}
+                    `
+                  );
+                  // clear inputs
+                  setSenderMail('');
+                  setTitle('');
+                  setBody('');
+                }}
+                className='flex flex-col gap-5 w-full'
+              >
+                <input
+                  type='text'
+                  className='border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#031926]'
+                  placeholder='Adınız'
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                <input
+                  type='email'
+                  className='border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#031926]'
+                  placeholder='ornek@mail.com'
+                  value={senderMail}
+                  onChange={(e) => setSenderMail(e.target.value)}
+                />
+                <textarea
+                  className='border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#031926]'
+                  placeholder='Mesajınız'
+                  value={body}
+                  onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <button
+                  type='submit'
+                  className='bg-[#031926] text-white font-semibold py-2 rounded-md'
+                >
+                  Gönder
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
