5100385fd3a972a28170b029f250b665b560f13e
Violetbee <caglarkarahuseyin28@gmail.com>
2023-06-02 23:02:26 +0300

    contact page ok

--- src/components/Header/Menu.js
+++ src/components/Header/Menu.js
@@ -1,89 +1,92 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { AiOutlinePhone, AiTwotoneHome } from "react-icons/ai";
-import { BsPatchQuestion } from "react-icons/bs";
-import { FaUmbrellaBeach } from "react-icons/fa";
-import { GiIsland } from "react-icons/gi";
-import { MdOutlinePhotoCameraBack, MdOutlineVilla } from "react-icons/md";
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { AiOutlinePhone, AiTwotoneHome } from 'react-icons/ai';
+import { BsPatchQuestion } from 'react-icons/bs';
+import { FaUmbrellaBeach } from 'react-icons/fa';
+import { GiIsland } from 'react-icons/gi';
+import { MdOutlinePhotoCameraBack, MdOutlineVilla } from 'react-icons/md';
 
 function Menu() {
   const slug = useRouter().route;
 
   return (
-    <div className="hidden w-full md:flex">
+    <div className='hidden w-full md:flex'>
       {/* box menu area, only react icons and hover box will color change */}
-      <div className="w-full rounded-md bg-[#031926] p-[3px]">
-        <div className="grid grid-cols-7 gap-7 rounded-md border-[2px] border-dashed border-white/10 p-2">
+      <div className='w-full rounded-md bg-[#031926] p-[3px]'>
+        <div className='grid grid-cols-7 gap-7 rounded-md border-[2px] border-dashed border-white/10 p-2'>
           <Link
-            href="/"
-            className="flex w-full flex-col items-center justify-center gap-1 rounded-md py-1 text-white duration-100 hover:bg-white/20"
+            href='/'
+            className='flex w-full flex-col items-center justify-center gap-1 rounded-md py-1 text-white duration-100 hover:bg-white/20'
           >
-            <span className="text-2xl">
+            <span className='text-2xl'>
               <AiTwotoneHome />
             </span>
-            <span className="text-lg tracking-wide">Ana Sayfa</span>
+            <span className='text-lg tracking-wide'>Ana Sayfa</span>
           </Link>
           {slug ? (
             <Link
-              href="/#aparts_section"
-              className="flex w-full flex-col items-center justify-center gap-1 rounded-md py-1 text-white duration-100 hover:bg-white/20"
+              href='/#aparts_section'
+              className='flex w-full flex-col items-center justify-center gap-1 rounded-md py-1 text-white duration-100 hover:bg-white/20'
             >
-              <span className="text-2xl">
+              <span className='text-2xl'>
                 <MdOutlineVilla />
               </span>
-              <span className="text-lg tracking-wide">Apartlar</span>
+              <span className='text-lg tracking-wide'>Apartlar</span>
             </Link>
           ) : (
             <a
-              href="#aparts_section"
-              className="flex w-full flex-col items-center justify-center gap-1 rounded-md py-1 text-white duration-100 hover:bg-white/20"
+              href='#aparts_section'
+              className='flex w-full flex-col items-center justify-center gap-1 rounded-md py-1 text-white duration-100 hover:bg-white/20'
             >
-              <span className="text-2xl">
+              <span className='text-2xl'>
                 <MdOutlineVilla />
               </span>
-              <span className="text-lg tracking-wide">Apartlar</span>
+              <span className='text-lg tracking-wide'>Apartlar</span>
             </a>
           )}
           <Link
-            href="/avsa-adasi"
-            className="flex w-full flex-col items-center justify-center gap-1 rounded-md py-1 text-white duration-100 hover:bg-white/20"
+            href='/avsa-adasi'
+            className='flex w-full flex-col items-center justify-center gap-1 rounded-md py-1 text-white duration-100 hover:bg-white/20'
           >
-            <span className="text-2xl">
+            <span className='text-2xl'>
               <GiIsland />
             </span>
-            <span className="text-lg tracking-wide">Avşa Adası</span>
+            <span className='text-lg tracking-wide'>Avşa Adası</span>
           </Link>
-          <button className="flex w-full flex-col items-center justify-center gap-1 rounded-md py-1 text-white duration-100 hover:bg-white/20">
-            <span className="text-2xl">
+          <button className='flex w-full flex-col items-center justify-center gap-1 rounded-md py-1 text-white duration-100 hover:bg-white/20'>
+            <span className='text-2xl'>
               <FaUmbrellaBeach />
             </span>
-            <span className="text-lg tracking-wide">Plajlar</span>
+            <span className='text-lg tracking-wide'>Plajlar</span>
           </button>
 
           <Link
-            href="/galeri"
-            className="flex w-full flex-col items-center justify-center gap-1 rounded-md py-1 text-white duration-100 hover:bg-white/20"
+            href='/galeri'
+            className='flex w-full flex-col items-center justify-center gap-1 rounded-md py-1 text-white duration-100 hover:bg-white/20'
           >
-            <span className="text-2xl">
+            <span className='text-2xl'>
               <MdOutlinePhotoCameraBack />
             </span>
-            <span className="text-lg tracking-wide">Galeri</span>
+            <span className='text-lg tracking-wide'>Galeri</span>
           </Link>
           <Link
-            href="/nasil-gidilir"
-            className="flex w-full flex-col items-center justify-center gap-1 rounded-md py-1 text-white duration-100 hover:bg-white/20"
+            href='/nasil-gidilir'
+            className='flex w-full flex-col items-center justify-center gap-1 rounded-md py-1 text-white duration-100 hover:bg-white/20'
           >
-            <span className="text-2xl">
+            <span className='text-2xl'>
               <BsPatchQuestion />
             </span>
-            <span className="text-lg tracking-wide">Nasıl Gidilir?</span>
+            <span className='text-lg tracking-wide'>Nasıl Gidilir?</span>
           </Link>
-          <button className="flex w-full flex-col items-center justify-between gap-1 rounded-md py-1 text-white duration-100 hover:bg-white/20">
-            <span className="text-2xl">
+          <Link
+            href='/iletisim'
+            className='flex w-full flex-col items-center justify-center gap-1 rounded-md py-1 text-white duration-100 hover:bg-white/20'
+          >
+            <span className='text-2xl'>
               <AiOutlinePhone />
             </span>
-            <span className="text-lg tracking-wide">İletişim</span>
-          </button>
+            <span className='text-lg tracking-wide'>İletişim</span>
+          </Link>
         </div>
       </div>
     </div>
--- src/pages/iletisim/index.js
+++ src/pages/iletisim/index.js
@@ -1,6 +1,40 @@
 import Layout from '@/components/Layout';
 
 function Index() {
-  return <Layout>index</Layout>;
+  return (
+    <Layout>
+      <div className='container mx-auto mt-3 max-w-[1300px] rounded-md bg-white p-1'>
+        <div className='flex flex-col justify-between gap-10 rounded-md border-[2px] border-dashed border-black/10 px-10 py-6 md:flex-row'>
+          {/* Contact Page, Including map and address */}
+          <div className='flex md:flex-row flex-col justify-center gap-10 text-zinc-900'>
+            <div className='flex flex-col gap-5 mt-5'>
+              <h1 className='text-4xl font-bold'>Adres</h1>
+              <p className='text-lg font-medium'>
+                Balıkesir/Marmara Avşa Adası Deniz Mahallesi Okullar
+                Caddesi No:50 Daire 8
+              </p>
+
+              {/* Phone */}
+              <h1 className='text-4xl font-bold'>Telefon</h1>
+              <p className='text-lg font-medium'>
+                <a href='tel:+905323456789'>+90 (536) 698 34 53</a>
+              </p>
+            </div>
+            <div className='relative w-full'>
+              <iframe
+                src='https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3033.351023618179!2d27.496068815398726!3d40.511731779354214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDDCsDMwJzQyLjIiTiAyN8KwMjknNTMuNyJF!5e0!3m2!1str!2str!4v1685181746791!5m2!1str!2str'
+                className='h-[500px] w-full rounded-md'
+                style={{ border: 0 }}
+                allowFullScreen
+                loading='lazy'
+              ></iframe>
+            </div>
+
+            {/* Address */}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 export default Index;
