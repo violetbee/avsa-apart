fe8e4a03051e6c57604e384d412155b3e2806783
Violetbee <caglarkarahuseyin28@gmail.com>
2023-06-02 18:54:52 +0300

    adv updates

--- src/components/Sections/Hero.js
+++ src/components/Sections/Hero.js
@@ -1,22 +1,22 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 function Hero() {
   return (
-    <section className="container mx-auto flex max-w-[1330px] md:-mt-5">
-      <div className="relative w-full">
+    <section className='container mx-auto flex max-w-[1330px] md:-mt-5'>
+      <div className='relative w-full'>
         <img
-          src="https://resimler.balnet.net/Konu/avsa/avsa2020/avsa-halk-plaji-1.JPG"
-          alt=""
-          className="h-[300px] w-full object-cover"
+          src='/images/avsa.jpg'
+          alt=''
+          className='h-[300px] w-full object-cover'
         />
-        <div className="absolute inset-0 flex flex-col items-center justify-center bg-black/30 p-2" />
+        <div className='absolute inset-0 flex flex-col items-center justify-center bg-black/30 p-2' />
 
         <Link
-          href="tel:05366983453"
-          target="_blank"
-          className="absolute bottom-2 right-2 bg-[#21A179] px-2 py-1 font-medium tracking-widest text-white"
+          href='tel:05366983453'
+          target='_blank'
+          className='absolute bottom-2 right-2 bg-[#21A179] px-2 py-1 font-medium tracking-widest text-white'
         >
-          Buraya Tıkla ve Hemen Rezervasyon Oluştur!
+          Rezervasyon Oluşturmak İçin Hemen Ara
         </Link>
       </div>
     </section>
