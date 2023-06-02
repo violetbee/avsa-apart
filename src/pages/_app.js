1c492db952e4cc59374e394913ac45f13d0ccd3c
Violetbee <caglarkarahuseyin28@gmail.com>
2023-06-02 14:58:09 +0300

    gtag ok

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,24 @@
+import Script from 'next/script';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        async
+        src='https://www.googletagmanager.com/gtag/js?id=AW-613698400'
+      ></Script>
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'AW-613698400');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
