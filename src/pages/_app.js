853519dd54f5089ce620935a112d82ec927ccc77
Violetbee <caglarkarahuseyin28@gmail.com>
2023-06-07 13:50:18 +0300

    ok

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,16 @@
+import Script from 'next/script';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Script id='google-analytics'>
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P8QSXXD');`}
+      </Script>
       <Component {...pageProps} />
     </>
   );
