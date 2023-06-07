f9ca6468344bad1b3802c47e0d232afdc2c480a5
Violetbee <caglarkarahuseyin28@gmail.com>
2023-06-07 13:22:23 +0300

    tag updated

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,23 +1,8 @@
-import Script from 'next/script';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Script
-        strategy='afterInteractive'
-        src='https://www.googletagmanager.com/gtag/js?id=AW-613698400'
-      ></Script>
-      <Script id='google-analytics' strategy='afterInteractive'>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'AW-613698400');
-        `}
-      </Script>
-
       <Component {...pageProps} />
     </>
   );
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='tr'>
+      <Head>
+        <Script id='google-analytics' strategy='lazyOnload'>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P8QSXXD');`}
+        </Script>
+      </Head>
       <body>
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-P8QSXXD'
+            height='0'
+            width='0'
+            style='display:none;visibility:hidden'
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
