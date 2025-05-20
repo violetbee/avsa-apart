035a47dca7377d2f970eaf39e29c6759e2f2347e
violetbee <caglarkarahuseyin28@gmail.com>
2025-05-20 21:32:16 +0300

    .

--- src/components/Sections/Hero.js
+++ src/components/Sections/Hero.js
@@ -1,24 +1,29 @@
-import Link from 'next/link';
+import { homePageGallery } from "@/utils/data";
+import { useState, useEffect } from "react";
 
 function Hero() {
-  return (
-    <section className='container mx-auto flex max-w-[1330px] md:-mt-5'>
-      <div className='relative w-full'>
-        <img
-          src='/images/avsa.jpg'
-          alt=''
-          className='h-[300px] w-full object-cover'
-        />
-        <div className='absolute inset-0 flex flex-col items-center justify-center bg-black/30 p-2' />
+  const [currentImage, setCurrentImage] = useState(0);
 
-        <Link
-          href='tel:05366983453'
-          target='_blank'
-          className='absolute bottom-2 right-2 bg-[#21A179] px-2 py-1 font-medium tracking-widest text-white'
-        >
-          Rezervasyon Oluşturmak İçin Hemen Ara
-        </Link>
-      </div>
+  useEffect(() => {
+    const galleryId = setInterval(() => {
+      if (currentImage === homePageGallery.length - 1) {
+        setCurrentImage(0);
+      } else {
+        setCurrentImage((prev) => prev + 1);
+      }
+    }, 3000);
+
+    return () => clearInterval(galleryId);
+  }, [currentImage]);
+
+  return (
+    <section className="container mx-auto flex max-w-[1330px] md:-mt-5">
+      <img
+        src={homePageGallery[currentImage]}
+        alt=""
+        className="h-[500px] w-full object-bottom object-cover"
+      />
+      {currentImage}
     </section>
   );
 }
